eb7dc3a55dcb9748510f080ae2ed3013555ebf4f
D G Sudheer <dgsudheer@gmail.com>
2025-06-02 23:53:51 +0530

    temp commit 2

--- src/Ludoboard.jsx
+++ src/Ludoboard.jsx
@@ -1,10 +1,16 @@
 import { useState } from "react";
+import "./Ludoboard.css";
 
 export default function Ludoboard({colour}){
     let styles = {backgroundColor: colour}
+    let [count, setCount] = useState(0);
+    let incCount = () => {
+        setCount(count+1);
+    }
+
     return (
-        <div style={styles}>
-            {colour}
+        <div className="Block" style={styles}>
+            <button onClick={incCount}>{colour} Count={count}</button>
         </div>
     );
 }
