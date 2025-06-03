61e910cdf58328507fc31adbabcdcac58187d37b
D G Sudheer <dgsudheer@gmail.com>
2025-06-03 22:07:01 +0530

    added colors

--- src/Ludoboard.jsx
+++ src/Ludoboard.jsx
@@ -2,12 +2,6 @@ import { useState } from "react";
 import "./Ludoboard.css";
 
 export default function Ludoboard({colour}){
-    let styles = {backgroundColor: colour}
-    /* let [count, setCount] = useState(0);
-    let incCount = () => {
-        setCount(count+1);
-    } */
-
     let [moves, setMoves] = useState({blue:0, red:0, green:0, yellow:0});
     let bCount = () => {
         setMoves((prev)=>{
@@ -36,13 +30,13 @@ export default function Ludoboard({colour}){
     return (
         <div>
             <p>Blue Moves={moves.blue}</p>
-            <button onClick={bCount}>Blue Count+1</button>
+            <button style={{backgroundColor: "blue"}} onClick={bCount}>Blue Count+1</button>
             <p>Red Moves={moves.red}</p>
-            <button onClick={rCount}>Red Count+1</button>
+            <button style={{backgroundColor: "red"}} onClick={rCount}>Red Count+1</button>
             <p>Green Moves={moves.green}</p>
-            <button onClick={gCount}>Green Count+1</button>
+            <button style={{backgroundColor: "green"}} onClick={gCount}>Green Count+1</button>
             <p>Yellow Moves={moves.yellow}</p>
-            <button onClick={yCount}>Yellow Count+1</button>
+            <button style={{backgroundColor: "yellow"}} onClick={yCount}>Yellow Count+1</button>
         </div>
     );
 }
