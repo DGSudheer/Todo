86121dd0d8f21776dfb90854fdd38afa163c80ad
D G Sudheer <dgsudheer@gmail.com>
2025-06-22 11:32:07 +0530

    basic form data handling

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
 
 export default function Form(){
+    let [fullName, setFullName]=useState("");
+    let [username, setUsername] = useState("");
+    let handleFullName = (event)=>{
+        console.log(`fullname:${fullname.value}`)
+        setFullName(event.target.value);
+    }
+    let handleUsername = (event)=>{
+        console.log(`username:${username.value}`);
+        setUsername(event.target.value);
+    }
     return(
         <div>
             <form action="/">
-                <input type="text" />
+                <label htmlFor="fullname"> Enter your Full Name</label> &nbsp;
+                <input type="text" placeholder="enter full name" id="fullname" value={fullName} onChange={handleFullName}/><br />
+                <label htmlFor="username">User Name</label>
+                <input type="text" placeholder="enter user name" id="username" value={username} onChange={handleUsername}></input>
                 <button>Submit</button>
             </form>
         </div>
